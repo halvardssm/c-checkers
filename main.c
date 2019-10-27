6d310e349d0e48ac1e8a0e1babca91ddf1bf1c08
halvardssm <halvard@simplyundoable.com>
2019-10-28 00:13:06 +0100

    adjusted structure, cleanup, modified logic

--- main.c
+++ main.c
@@ -17,28 +17,25 @@
 #include <string.h>
 #include <stdbool.h>
 
-#define EMPTY 0
-#define P1 1
-#define P1K 2
-#define P2 3
-#define P2K 4
-
 typedef struct Player {
-    int pawnValue;
-    int kingValue;
+    int id;
     int direction; //1 for p1, -1 for p2
+    int validMoves;
 } Player;
 
 typedef struct BrickMovement {
-    int coX;
-    int coY;
+    int cX;
+    int cY;
     int priority; //0: not valid, 1: step, 2: jump
+    char printable[2];
 } BrickMovement;
 
 typedef struct PlayerBrick {
-    int type;
-    int x;
-    int y;
+    int id;
+    int cX;
+    int cY;
+    int playerId;
+    bool isKing;
     BrickMovement brickMovement[4];
 } PlayerBrick;
 
@@ -48,130 +45,126 @@ typedef struct GameState {
     Player allPlayers[2];
     Player currentPlayer;
     int boardArray[8][8];
+    bool gameIsOn;
 } GameState;
 
-Player player1 = {.pawnValue=1, .kingValue=2, .direction=1};
-Player player2 = {.pawnValue=3, .kingValue=4, .direction=-1};
+Player player1 = {.id=0, .direction=1, .validMoves=0};
+Player player2 = {.id=1, .direction=-1, .validMoves=0};
 
 GameState GAMES_STATE = {
-        .playerBricks={
-                {1, 0, 1, 0},
-                {1, 0, 3, 0},
-                {1, 0, 5, 0},
-                {1, 0, 7, 0},
-                {1, 1, 0, 0},
-                {1, 1, 2, 0},
-                {1, 1, 4, 0},
-                {1, 1, 6, 0},
-                {1, 2, 1, 0},
-                {1, 2, 3, 0},
-                {1, 2, 5, 0},
-                {1, 2, 7, 0},
-                {3, 5, 0, 0},
-                {3, 5, 2, 0},
-                {3, 5, 4, 0},
-                {3, 5, 6, 0},
-                {3, 6, 1, 0},
-                {3, 6, 3, 0},
-                {3, 6, 5, 0},
-                {3, 6, 7, 0},
-                {3, 7, 0, 0},
-                {3, 7, 2, 0},
-                {3, 7, 4, 0},
-                {3, 7, 6, 0},
+        .playerBricks={ //id starts at 1 to give the value 0 = empty
+                {1,  1, 0, 0, false, 0},
+                {2,  3, 0, 0, false, 0},
+                {3,  5, 0, 0, false, 0},
+                {4,  7, 0, 0, false, 0},
+                {5,  0, 1, 0, false, 0},
+                {6,  2, 1, 0, false, 0},
+                {7,  4, 1, 0, false, 0},
+                {8,  6, 1, 0, false, 0},
+                {9,  1, 2, 0, false, 0},
+                {10, 3, 2, 0, false, 0},
+                {11, 5, 2, 0, false, 0},
+                {12, 7, 2, 0, false, 0},
+                {13, 0, 5, 1, false, 0},
+                {14, 2, 5, 1, false, 0},
+                {15, 4, 5, 1, false, 0},
+                {16, 6, 5, 1, false, 0},
+                {17, 1, 6, 1, false, 0},
+                {18, 3, 6, 1, false, 0},
+                {19, 5, 6, 1, false, 0},
+                {20, 7, 6, 1, false, 0},
+                {21, 0, 7, 1, false, 0},
+                {22, 2, 7, 1, false, 0},
+                {23, 4, 7, 1, false, 0},
+                {24, 6, 7, 1, false, 0},
         },
         .boardArray={0},
         .allPlayers={
-                {.pawnValue=1, .kingValue=2, .direction=1},
-                {.pawnValue=3, .kingValue=4, .direction=-1}
+                {.id=0, .direction=1, .validMoves=0},
+                {.id=1, .direction=-1, .validMoves=0}
         },
-        .currentPlayer={.pawnValue=1, .kingValue=2, .direction=1},
-        .currentBrick={0, 0, 0}
+        .currentPlayer={.id=0, .direction=1, .validMoves=0},
+        .currentBrick={0},
+        .gameIsOn=true
 };
 
-bool brickIsCurrentPlayer(int playerType) {
-    if (playerType == GAMES_STATE.currentPlayer.pawnValue ||
-        playerType == GAMES_STATE.currentPlayer.kingValue) {
-        return true;
-    }
-    return false;
+int getDirectionOfBrick(PlayerBrick brick) {
+    int id = brick.playerId;
+
+    return GAMES_STATE.allPlayers[id].direction;
 }
 
-bool playerIsCurrentPlayer(Player player) {
-    if (GAMES_STATE.currentPlayer.pawnValue == player.pawnValue &&
-        GAMES_STATE.currentPlayer.kingValue == player.kingValue) {
-        return true;
-    }
-    return false;
-}
-
-bool brickIsPawn(PlayerBrick brick) {
-    if (brick.type == player1.pawnValue && brick.type == player2.pawnValue) {
-        return true;
-    }
-    return false;
-}
-
-bool brickIsKing(PlayerBrick brick) {
-    if (brick.type == player1.kingValue && brick.type == player2.kingValue) {
-        return true;
-    }
-    return false;
-}
-
-void generateBoard(char board[1000]) {
+void generateBoard() {
     int cX = 0;
     int cY = 0;
 
     while (cY < 8) {
-        strcat(board, "   ---------------------------------\n");
-        char iY[5];
-        sprintf(iY, " %d |", cY);
-        strcat(board, iY);
+        printf("   ---------------------------------\n");
+        printf(" %d |", cY);
 
         while (cX < 8) {
+            int brickId = GAMES_STATE.boardArray[cX][cY];
 
-            switch (GAMES_STATE.boardArray[cX][cY]) {
-                case 0:
-                    strcat(board, "   |");
-                    break;
-                case 1:
-                    strcat(board, " o |");
-                    break;
-                case 2:
-                    strcat(board, " 0 |");
-                    break;
-                case 3:
-                    strcat(board, " x |");
-                    break;
-                case 4:
-                    strcat(board, " + |");
-                    break;
+            if (brickId == 0) {
+                printf("   |");
 
-                default:
-                    break;
+            } else {
+                PlayerBrick brick = GAMES_STATE.playerBricks[brickId - 1];
+
+                if (brick.playerId == player1.id) {
+                    if (brick.isKing) {
+                        printf(" 0 |");
+                        continue;
+                    }
+                    printf(" o |");
+
+                } else {
+
+                    if (brick.isKing) {
+                        printf(" + |");
+                        continue;
+                    }
+                    printf(" x |");
+                }
             }
-
             cX++;
         }
         cX = 0;
-        strcat(board, "\n");
+        printf("\n");
         cY++;
     }
-    strcat(board, "   ---------------------------------\n");
-    strcat(board, "     A   B   C   D   E   F   G   H  \n\n");
-    strcat(board, "    P1(king): o(0)   P2(king): x(+) \n\n\0");
+    printf("   ---------------------------------\n");
+    printf("     A   B   C   D   E   F   G   H  \n\n");
+    printf("    P1(king): o(0)   P2(king): x(+) \n\n");
+}
+
+void getBoardArray() {
+    for (int i = 0; i < 24; i++) {
+        int cX = GAMES_STATE.playerBricks[i].cX;
+        int cY = GAMES_STATE.playerBricks[i].cY;
+        GAMES_STATE.boardArray[cX][cY] = GAMES_STATE.playerBricks[i].id;
+    }
 }
 
 int inputIsValid(int inputX, int inputY) {
 
     if (inputX >= 0 && inputX < 8 && inputY < 8 && inputY >= 0) {
-        for (int i = 0; i < 24; i++) {
+        int brickId = GAMES_STATE.boardArray[inputX][inputY];
 
-            if (GAMES_STATE.playerBricks[i].x == inputX && GAMES_STATE.playerBricks[i].y == inputY &&
-                brickIsCurrentPlayer(GAMES_STATE.playerBricks[i].type)) {
-                GAMES_STATE.currentBrick = GAMES_STATE.playerBricks[i];
-                return true;
+        if (brickId == 0) {
+            return false;
+        }
+
+        PlayerBrick brick = GAMES_STATE.playerBricks[brickId - 1];
+
+        if (brick.playerId == GAMES_STATE.currentPlayer.id) {
+
+            for (int i = 0; i < 4; i++) {
+
+                if (brick.brickMovement[i].priority != 0) {
+                    GAMES_STATE.currentBrick = brick;
+                    return true;
+                }
             }
         }
     }
@@ -179,115 +172,49 @@ int inputIsValid(int inputX, int inputY) {
     return false;
 }
 
-void gBoardArray(int boardArray[8][8]) {
-
-    int cX = 0;
-    int cY = 0;
-
-    char bricks[4] = {P1, P1K, P2, P2K};
-
-    while (cY < 8) {
-        while (cX < 8) {
-            int player = 0;
-            switch (cY) {
-                case 0:
-                case 2:
-                case 6:
-                    if (cY > 3) {
-                        player += 2;
-                    }
-
-                    switch (cX) {
-                        case 1:
-                        case 3:
-                        case 5:
-                        case 7:
-                            boardArray[cX][cY] = player + 1;
-                            break;
-
-                        default:
-                            boardArray[cX][cY] = 0;
-                            break;
-                    }
-                    break;
-
-                case 1:
-                case 5:
-                case 7:
-                    switch (cX) {
-                        case 0:
-                        case 2:
-                        case 4:
-                        case 6:
-                            if (cY < 4) {
-                                boardArray[cX][cY] = 1;
-                            } else {
-                                boardArray[cX][cY] = 3;
-                            }
-
-                            break;
-
-                        default:
-                            boardArray[cX][cY] = 0;
-                            break;
-                    }
-                    break;
-
-                default:
-                    boardArray[cX][cY] = 0;
-                    break;
-            }
-
-            cX++;
-        }
-        cX = 0;
-        cY++;
-    }
+void updateBrickMovement(int brickIndex, int brickMovement, int cX, int cY, int pri) {
+    GAMES_STATE.playerBricks[brickIndex].brickMovement[brickMovement].cX = cX;
+    GAMES_STATE.playerBricks[brickIndex].brickMovement[brickMovement].cY = cY;
+    GAMES_STATE.playerBricks[brickIndex].brickMovement[brickMovement].priority = pri;
+    GAMES_STATE.playerBricks[brickIndex].brickMovement[brickMovement].printable[0] = cX + 65;
+    GAMES_STATE.playerBricks[brickIndex].brickMovement[brickMovement].printable[1] = cY + 48;
+    GAMES_STATE.allPlayers[GAMES_STATE.playerBricks[brickIndex].playerId].validMoves++;
 }
 
-void getBoardArray() {
-
-    for (int i = 0; i < 24; i++) {
-        int coX = GAMES_STATE.playerBricks[i].x;
-        int coY = GAMES_STATE.playerBricks[i].y;
-        GAMES_STATE.boardArray[coX][coY] = GAMES_STATE.playerBricks[i].type;
-    }
-}
-
-int getStatusOfMovementForBrick(int brickIndex, int coX, int coY, int brickMovement) {
-    int direction = GAMES_STATE.currentPlayer.direction;
+int getStatusOfMovementForBrick(int brickIndex, int cX, int cY, int brickMovement) {
+    int direction = getDirectionOfBrick(GAMES_STATE.playerBricks[brickIndex]);
     PlayerBrick brick = GAMES_STATE.playerBricks[brickIndex];
 
-    int closeX = brick.x + (coX * direction);
-    int closeY = brick.x + (coY * direction);
-    int farX = brick.x + (coX * 2 * direction);
-    int farY = brick.x + (coY * 2 * direction);
+    int closeX = brick.cX + cX;
+    int closeY = brick.cY + cY;
+    int farX = brick.cX + (cX * 2);
+    int farY = brick.cY + (cY * 2);
 
-    if (closeX >= 0 && closeX < 8 && closeY >= 0 && closeY < 8) {
-        int closeCell = GAMES_STATE.boardArray[closeY][closeX];
+    if (direction == 0) {
+        return 0;
+    } else if (closeX >= 0 && closeX < 8 && closeY >= 0 && closeY < 8) {
+        int brickId = GAMES_STATE.boardArray[closeX][closeY];
+
+        bool directionIsValid = (direction * cY) > 0;
 
 //        Check if the brick can move in this direction
-        if (brickIsPawn(brick) && (direction * coY) < 0) {
+        if (!brick.isKing && !directionIsValid) {
             return 0;
 
-        } else if (brickIsCurrentPlayer(closeCell)) {
+        } else if (brickId != 0 && GAMES_STATE.playerBricks[brickId - 1].playerId == brick.playerId) {
             return 0;
 
-        } else if (closeCell == 0) {
-            brick.brickMovement[brickMovement].coY = closeY;
-            brick.brickMovement[brickMovement].coX = closeX;
-            brick.brickMovement[brickMovement].priority = 1;
+        } else if (brickId == 0) {
+            updateBrickMovement(brickIndex, brickMovement, closeX, closeY, 1);
             return 1;
 
         } else {
 //            if it reaches here, it is an enemy player, and we have to check if we can jump over it
             if (farX >= 0 && farX < 8 && farY >= 0 && farY < 8) {
-                int farCell = GAMES_STATE.boardArray[farY][farX];
+                int farCell = GAMES_STATE.boardArray[farX][farY];
 
                 if (farCell == 0) {
-                    brick.brickMovement[brickMovement].coY = farX;
-                    brick.brickMovement[brickMovement].coX = farY;
-                    brick.brickMovement[brickMovement].priority = 2;
+                    updateBrickMovement(brickIndex, brickMovement, farX, farY, 2);
                     return 2;
                 }
             }
@@ -296,70 +223,116 @@ int getStatusOfMovementForBrick(int brickIndex, int coX, int coY, int brickMovem
     return 0;
 }
 
-void getValidMovementsForBrick(int brickIndex) {
-    // 1 for p1, -1 for p2, 0 for kings
-
-//  Check corners of brick (N and S is reversed for p2)
-//  0 = blocked, 1 = step, 2 = jump
-    getStatusOfMovementForBrick(brickIndex, 1, -1, 0);
-    getStatusOfMovementForBrick(brickIndex, 1, 1, 1);
-    getStatusOfMovementForBrick(brickIndex, -1, 1, 2);
-    getStatusOfMovementForBrick(brickIndex, -1, -1, 3);
-
-
-}
-
 void getValidityOfBricks() {
+    GAMES_STATE.allPlayers[0].validMoves = 0;
+    GAMES_STATE.allPlayers[1].validMoves = 0;
+
     for (int i = 0; i < 24; i++) {
 
-        if (brickIsCurrentPlayer(GAMES_STATE.playerBricks[i].type)) {
-            getValidMovementsForBrick(i);
-        }
+        getStatusOfMovementForBrick(i, 1, -1, 0);
+        getStatusOfMovementForBrick(i, 1, 1, 1);
+        getStatusOfMovementForBrick(i, -1, 1, 2);
+        getStatusOfMovementForBrick(i, -1, -1, 3);
+
+
     }
 }
 
+void printValidOutputCellsForBrick(BrickMovement *brickMovement) {
+//    GAMES_STATE.currentBrick.brickMovement[i].cX+65,GAMES_STATE.currentBrick.brickMovement[i].cY+48
+    printf("[");
+    for (int i = 0; i < 4; i++) {
+        printf("%s", brickMovement[i].printable);
+        printf("|");
+    }
+    printf("]\n");
+}
+
+bool inputToIsValidForBrick(PlayerBrick brick, char inputTo[2]) {
+    for (int i = 0; i < 4; i++) {
+        if (brick.brickMovement[i].printable[0] == inputTo[0] && brick.brickMovement[i].printable[1] == inputTo[1]) {
+            return true;
+        }
+    }
+    return false;
+}
+
 void adjustPlayerMovement() {
-    char inputX[1];
-    int inputY[1];
+    char inputSelect[2];
     bool correctInput = false;
 
     while (!correctInput) {
-        getValidityOfBricks();
         printf(" Select brick to move (ex: A2):\n");
 
-        scanf("%c%d", inputX, inputY);
+        scanf("%s", inputSelect);
 
-
-        bool isValidPlayerBrick = inputIsValid(inputX[0] - 65, inputY[0]);
+        bool isValidPlayerBrick = inputIsValid(inputSelect[0] - 65, inputSelect[1] - 48);
 
         if (isValidPlayerBrick) {
+            char inputTo[2];
+            printf(" Move brick to one of cells: ");
+            printValidOutputCellsForBrick(GAMES_STATE.currentBrick.brickMovement);
 
-            printf(" Move brick to (ex: A2):\n");
+            scanf("%s", inputTo);
+
+            bool inputToIsValid = inputToIsValidForBrick(GAMES_STATE.currentBrick, inputTo);
+            if (!inputToIsValid) {
+                printf(" Please choose a valid movement\n");
+                continue;
+            }
+
+            GAMES_STATE.playerBricks[GAMES_STATE.currentBrick.id - 1].cX = inputTo[0] - 65;
+            GAMES_STATE.playerBricks[GAMES_STATE.currentBrick.id - 1].cY = inputTo[1] - 48;
+
+            correctInput = true;
         } else {
-
             printf(" Please choose a valid brick\n");
         }
     }
 }
 
+void checkIfGameOver() {
+    int p1ValidMoves = GAMES_STATE.allPlayers[player1.id].validMoves;
+    int p2ValidMoves = GAMES_STATE.allPlayers[player2.id].validMoves;
+
+    if (p1ValidMoves < 1 || p2ValidMoves < 1) {
+        char *winner;
+        bool p1Wins = p1ValidMoves > p2ValidMoves;
+
+        if (p1Wins) {
+            winner = "Player 1";
+        } else {
+            winner = "Player 2";
+        }
+
+        GAMES_STATE.gameIsOn = false;
+        printf("#### %s is the winner!!! ####", winner);
+    }
+}
+
+void switchCurrentPlayer() {
+    if (GAMES_STATE.currentPlayer.id == player1.id) {
+        GAMES_STATE.currentPlayer = player2;
+    } else {
+        GAMES_STATE.currentPlayer = player1;
+    }
+}
+
 int main(int argc, const char *argv[]) {
 
-    getBoardArray();
+    while (GAMES_STATE.gameIsOn) {
 
-    char board[1000];
+        getBoardArray();
+        generateBoard();
 
-    generateBoard((char *) board);
-
-    printf("%s", board);
-
-    bool gameOn = true;
-
-    while (gameOn) {
+        getValidityOfBricks();
 
         adjustPlayerMovement();
 
-    }
+        checkIfGameOver();
 
+        switchCurrentPlayer();
+    }
 
     return 0;
 }
